a7e89de8b169217276c2dac72dc1cf68e0b5d206
felipe <feliperodriguesqueiroz122@gmail.com>
2024-05-26 01:55:38 -0300

    feat: Titulo e subtitulo do site acrescentados

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,56 @@ import HeaderHome from '../components/Header.vue'
 </script>
 
 <template>
-<section></section>
+<div class="pageTitle">
+    <p class="Title">Furz<br>parfum</p>
+</div>
+<div class="subTitle">
+    <p>Desperte seus sentidos, libere a sua essência</p>
+</div>
 </template>
 
 <style>
+
+@font-face {
+    font-family: PetitFormal;
+    src: url(../assets/fonts/PetitFormalScript-Regular.ttf)
+}
+@font-face {
+    font-family: Kurale;
+    src: url(../assets/fonts/Kurale-Regular.ttf);
+}
 body {
     background-image: url(../assets/images/landingpage.png);
     background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+    height: 100vh;
+}
+.pageTitle {
+    position: absolute;
+    top: 30%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+}
+
+.Title {
+    font-family: PetitFormal, sans-serif;
+    font-size: 170px;
+    color: #000; 
+    line-height: 0.9;
+}
+.subTitle {
+    font-family: Kurale;
+    font-size: 50px;
+    position: absolute;
+    top:53%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
     
+
 }
 </style>
