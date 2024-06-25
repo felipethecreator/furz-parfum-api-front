009f7e2eb839a6a2113938de85481d5fda94e493
felipe <feliperodriguesqueiroz122@gmail.com>
2024-06-25 01:00:13 -0300

    feat: Criando nova rota para a main page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home.vue'
 import LoginView from '../views/LoginPage.vue' 
 import RegisterView from '../views/RegisterPage.vue'
+import MainPage from '../views/mainPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +18,10 @@ const router = createRouter({
     {
       path: '/register',
       component: RegisterView
+    },
+    {
+      path: '/page',
+      component: MainPage
     }
   ]
 })
