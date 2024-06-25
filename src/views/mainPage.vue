e5a1be9fc9d83061c4a7cf046d27bdf2ff9af32c
felipe <feliperodriguesqueiroz122@gmail.com>
2024-06-25 01:02:04 -0300

    feat: Criando view mainPage

---
+++ src/views/mainPage.vue
@@ -0,0 +1,18 @@
+<script></script>
+
+<template>
+    <div>
+        <p class="logado">
+            parabens, voce logou
+        </p>
+    </div>
+</template>
+
+<style>
+
+.logado {
+    font-size: larger;
+    color: blue;
+}
+
+</style>
