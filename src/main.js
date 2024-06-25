3bbf261e13d67a510e804753b88ecbde6500722a
felipe <feliperodriguesqueiroz122@gmail.com>
2024-06-25 00:56:10 -0300

    chore: Importando vue-toastification para o projeto

--- src/main.js
+++ src/main.js
@@ -4,9 +4,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Toast, { POSITION } from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 
 const app = createApp(App)
 
 app.use(router)
+
+app.use(Toast, {
+    position: POSITION.TOP_RIGHT,
+  });
+  
 
 app.mount('#app')
